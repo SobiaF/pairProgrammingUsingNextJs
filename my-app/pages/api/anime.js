62937c62c30527f2d8253b5896774d80fc2357ca
Sobia Furqan <sobiafurqan@hotmail.com>
2021-07-24 15:25:43 +0100

    removed a comment

--- my-app/pages/api/anime.js
+++ my-app/pages/api/anime.js
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default async function handler(req, res) {
   const response = await fetch('https://animechan.vercel.app/api/random');
   const quote = await response.json();
